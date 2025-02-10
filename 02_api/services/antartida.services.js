ffdc99129b733df511b047273f5788ad99f05087
David <david.perez.campos82@gmail.com>
2025-02-10 20:46:00 +0100

    Validations restructuring so beautiful

--- 02_api/services/antartida.services.js
+++ 02_api/services/antartida.services.js
@@ -11,31 +11,43 @@ export default class AntartidaServices{
     static async getAntartida(fechaini, fechafin, estacion, respCode){
         // Call getAntartida endpoint
         const RESP = await apis.getAntartida(fechaini, fechafin, estacion);
-        // Validate response status code
-        expect(RESP.status).toEqual(respCode);
         // Validate response date
+        this.validateRespData(RESP, 200);
+        // Validate response body structure
+        this.validateRespBodyStructure(RESP.data, expectedAntartida);
+        // Call JSON data endpoint
+        const RESP_JSON = await apis.getJson(RESP.data.datos);
+        // Validate JSON data status code + response keys
+        this.validateJsonData(expectedDatos, RESP_JSON, 200);
+    }
+
+    static validateRespData(resp, statusCode){
+        expect(resp.status).toEqual(statusCode);
+        console.log(`✅ Status code ${statusCode} in response data.`);
         const UTC_TIME = utils.getUtcTime();
-        const REQ_SECS = utils.convertUtcTimeToSecs(RESP.headers.date);
+        const REQ_SECS = utils.convertUtcTimeToSecs(resp.headers.date);
         const RESP_SECS = utils.convertUtcTimeToSecs(UTC_TIME);
         const timeDifference = Math.abs(REQ_SECS - (RESP_SECS));
         expect(timeDifference).toBeLessThanOrEqual(2);
-        expect(RESP.headers.date.replace(utils.getTimeFromUTCString(RESP.headers.date), '')).toEqual(UTC_TIME.replace(utils.getTimeFromUTCString(UTC_TIME), ''));
-        // Validate response body structure
-        const RESP_DATA = Object.keys(RESP.data);
-        const EXP_DATA = Object.keys(expectedAntartida);
-        expect(RESP_DATA.sort()).toEqual(EXP_DATA.sort());
-        console.log(`✅ ${RESP_DATA.length} total items validated in response body.`);
+        expect(resp.headers.date.replace(utils.getTimeFromUTCString(resp.headers.date), '')).toEqual(UTC_TIME.replace(utils.getTimeFromUTCString(UTC_TIME), ''));
+    }
 
-        // Call JSON data endpoint
-        const RESP_JSON = await apis.getJson(RESP.data.datos);
-        const EXP_JSON = Object.keys(expectedDatos);
-        // Validate JSON data status code + response keys
-        expect(RESP_JSON.status).toEqual(200);
-        RESP_JSON.data.forEach((item) => {
+    static validateRespBodyStructure(resp, expected){
+        const RESP_DATA = Object.keys(resp);
+        const EXP_DATA = Object.keys(expected);
+        expect(RESP_DATA.sort()).toEqual(EXP_DATA.sort());
+        console.log(`✅ ${RESP_DATA.length} total items validated in response body structure.`);
+    }
+
+    static validateJsonData(expected, resp, statusCode){
+        const EXP_JSON = Object.keys(expected);
+        expect(resp.status).toEqual(statusCode);
+        console.log(`✅ Status code ${statusCode} in JSON data.`);
+        resp.data.forEach((item) => {
             const RESP_KEYS = Object.keys(item).sort();
             const EXP_KEYS = EXP_JSON.sort();
             expect(RESP_KEYS).toEqual(EXP_KEYS);
         });
-        console.log(`✅ ${RESP_JSON.data.length} total items validated in JSON.`);
+        console.log(`✅ ${resp.data.length} total items validated in JSON structure.`);
     }
 }
